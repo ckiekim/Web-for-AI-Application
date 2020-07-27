3207b0e24d934a906317a0696f352e4a86de6cf4
Cheolkie Kim <ckiekim@naver.com>
2020-07-27 14:58:45 +0900

    감성분석

--- Project/app.py
+++ Project/app.py
@@ -2,11 +2,41 @@ from flask import Flask, render_template, request
 import os
 import joblib
 import numpy as np
+import pandas as pd
+import re
+from konlpy.tag import Okt
 from tensorflow import keras
 from keras.models import load_model
 
 app = Flask(__name__)
 app.debug = True
+
+okt = Okt()
+movie_lr = None
+movie_lr_dtm = None
+def load_movie_lr():
+    global movie_lr, movie_lr_dtm
+    movie_lr = joblib.load(os.path.join(app.root_path, 'model/movie_lr.pkl'))
+    movie_lr_dtm = joblib.load(os.path.join(app.root_path, 'model/movie_lr_dtm.pkl'))
+
+def tw_tokenizer(text):
+    # 입력 인자로 들어온 text 를 형태소 단어로 토큰화 하여 list 객체 반환
+    tokens_ko = okt.morphs(text)
+    return tokens_ko
+
+movie_nb = None
+movie_nb_dtm = None
+def load_movie_nb():
+    global movie_nb, movie_nb_dtm
+    movie_nb = joblib.load(os.path.join(app.root_path, 'model/movie_nb.pkl'))
+    movie_nb_dtm = joblib.load(os.path.join(app.root_path, 'model/movie_nb_dtm.pkl'))
+
+def nb_transform(review):
+    stopwords=['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+    review = review.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+    morphs = okt.morphs(review, stem=True)
+    temp = ' '.join(morph for morph in morphs if not morph in stopwords)
+    return temp
 
 model_iris_lr = None
 model_iris_svm = None
@@ -41,9 +71,25 @@ def regression():
         iris = {'slen':slen, 'swid':swid, 'plen':plen, 'pwid':pwid, 'species':species}
         return render_template('reg_result.html', menu=menu, iris=iris)
 
-@app.route('/sentiment')
+@app.route('/sentiment', methods=['GET', 'POST'])
 def sentiment():
-    pass
+    menu = {'home':False, 'rgrs':False, 'stmt':True, 'clsf':False, 'clst':False, 'user':False}
+    if request.method == 'GET':
+        return render_template('sentiment.html', menu=menu)
+    else:
+        res_str = ['부정', '긍정']
+        review = request.form['review']
+        # Logistic Regression 처리
+        review_lr = re.sub(r"\d+", " ", review)
+        review_lr_dtm = movie_lr_dtm.transform([review_lr])
+        result_lr = res_str[movie_lr.predict(review_lr_dtm)[0]]
+        # Naive Bayes 처리
+        review_nb = nb_transform(review)
+        review_nb_dtm = movie_nb_dtm.transform([review_nb])
+        result_nb = res_str[movie_nb.predict(review_nb_dtm)[0]]
+        # 결과 처리
+        movie = {'review':review, 'result_lr':result_lr, 'result_nb':result_nb}
+        return render_template('senti_result.html', menu=menu, movie=movie)
 
 @app.route('/classification')
 def classification():
@@ -75,5 +121,7 @@ def clustering():
     pass
 
 if __name__ == '__main__':
+    load_movie_lr()
+    load_movie_nb()
     load_iris()
     app.run()
