4bf1a2f2b320a315b8c23a9066698e776f4262c1
Cheolkie Kim <ckiekim@naver.com>
2020-07-27 12:34:07 +0900

    Iris 분류

--- Project/app.py
+++ Project/app.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template, request
+import os
+import joblib
+import numpy as np
+from tensorflow import keras
+from keras.models import load_model
+
 app = Flask(__name__)
+app.debug = True
+
+model_iris_lr = None
+model_iris_svm = None
+model_iris_dt = None
+model_iris_deep = None
+def load_iris():
+    global model_iris_lr, model_iris_svm, model_iris_dt, model_iris_deep
+    model_iris_lr = joblib.load(os.path.join(app.root_path, 'model/iris_lr.pkl'))
+    model_iris_svm = joblib.load(os.path.join(app.root_path, 'model/iris_svm.pkl'))
+    model_iris_dt = joblib.load(os.path.join(app.root_path, 'model/iris_dt.pkl'))
+    model_iris_deep = load_model(os.path.join(app.root_path, 'model/iris_deep.hdf5'))
 
 @app.route('/')
 def index():
@@ -37,11 +55,25 @@ def classification_iris():
     if request.method == 'GET':
         return render_template('classification_iris.html', menu=menu)
     else:
-        pass
+        sp_names = ['Setosa', 'Versicolor', 'Virginica']
+        slen = float(request.form['slen'])      # Sepal Length
+        swid = float(request.form['swid'])      # Sepal Width
+        plen = float(request.form['plen'])      # Petal Length
+        pwid = float(request.form['pwid'])      # Petal Width
+        test_data = np.array([slen, swid, plen, pwid]).reshape(1,4)
+        species_lr = sp_names[model_iris_lr.predict(test_data)[0]]
+        species_svm = sp_names[model_iris_svm.predict(test_data)[0]]
+        species_dt = sp_names[model_iris_dt.predict(test_data)[0]]
+        species_deep = sp_names[model_iris_deep.predict_classes(test_data)[0]]
+        iris = {'slen':slen, 'swid':swid, 'plen':plen, 'pwid':pwid, 
+                'species_lr':species_lr, 'species_svm':species_svm,
+                'species_dt':species_dt, 'species_deep':species_deep}
+        return render_template('cla_iris_result.html', menu=menu, iris=iris)
 
 @app.route('/clustering')
 def clustering():
     pass
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    load_iris()
+    app.run()
